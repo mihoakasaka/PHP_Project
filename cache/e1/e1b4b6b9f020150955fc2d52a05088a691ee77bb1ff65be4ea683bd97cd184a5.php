c3d704e8f825dde0811dfb530594c00bb697cf87
BH210-15\ipd <miho>
2017-11-03 10:23:19 -0400

    *PHP_Project created  register.html.twig added to test database connection

---
+++ cache/e1/e1b4b6b9f020150955fc2d52a05088a691ee77bb1ff65be4ea683bd97cd184a5.php
@@ -0,0 +1,155 @@
+<?php
+
+/* register.html.twig */
+class __TwigTemplate_bad9f3f9ef7bb6cc18cfd77633e467e37a8c076747f1d37693fa4259450ab0c8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("master.html.twig", "register.html.twig", 1);
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'headextra' => array($this, 'block_headextra'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "master.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Resister";
+    }
+
+    // line 3
+    public function block_headextra($context, array $blocks = array())
+    {
+        echo " 
+    <script>
+        \$(document).ready(function () {
+            \$('input[name=email]').bind('propertychange change click keyup input paste', function () {
+                // AJAX request
+                var email = \$('input[name=email]').val();
+                \$('#isTaken').load('/isemailregistered/' + email);
+            });
+        });
+    </script>
+";
+    }
+
+    // line 15
+    public function block_content($context, array $blocks = array())
+    {
+        // line 16
+        echo "        ";
+        if ((isset($context["errorList"]) ? $context["errorList"] : null)) {
+            // line 17
+            echo "        <ul>
+            ";
+            // line 18
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((isset($context["errorList"]) ? $context["errorList"] : null));
+            foreach ($context['_seq'] as $context["_key"] => $context["error"]) {
+                // line 19
+                echo "                <li>";
+                echo twig_escape_filter($this->env, $context["error"], "html", null, true);
+                echo "</li>
+                ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['error'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 21
+            echo "        </ul>
+        ";
+        }
+        // line 23
+        echo "
+            <form method=\"post\">
+                Name: <input type=\"text\" name=\"name\" value=\"";
+        // line 25
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "name", array()), "html", null, true);
+        echo "\"><br>
+                Email: <input type=\"email\" name=\"email\"  value=\"";
+        // line 26
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "email", array()), "html", null, true);
+        echo "\"> <span id=\"isTaken\"></span><br>
+
+                Password: <input type=\"password\" name=\"pass1\"><br>
+                Password (repeated): <input type=\"password\" name=\"pass2\"><br>
+                <input type=\"submit\" value=\"Register\">
+            </form>
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "register.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  86 => 26,  82 => 25,  78 => 23,  74 => 21,  65 => 19,  61 => 18,  58 => 17,  55 => 16,  52 => 15,  36 => 3,  30 => 2,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends \"master.html.twig\" %}
+{% block title %}Resister{% endblock %}
+{% block headextra %} 
+    <script>
+        \$(document).ready(function () {
+            \$('input[name=email]').bind('propertychange change click keyup input paste', function () {
+                // AJAX request
+                var email = \$('input[name=email]').val();
+                \$('#isTaken').load('/isemailregistered/' + email);
+            });
+        });
+    </script>
+{% endblock %}
+
+  {% block content %}
+        {% if errorList %}
+        <ul>
+            {% for error in errorList %}
+                <li>{{error}}</li>
+                {% endfor %}
+        </ul>
+        {% endif %}
+
+            <form method=\"post\">
+                Name: <input type=\"text\" name=\"name\" value=\"{{v.name}}\"><br>
+                Email: <input type=\"email\" name=\"email\"  value=\"{{v.email}}\"> <span id=\"isTaken\"></span><br>
+
+                Password: <input type=\"password\" name=\"pass1\"><br>
+                Password (repeated): <input type=\"password\" name=\"pass2\"><br>
+                <input type=\"submit\" value=\"Register\">
+            </form>
+    {% endblock %} 
+", "register.html.twig", "C:\\xampp\\htdocs\\PHP_Project\\templates\\register.html.twig");
+    }
+}
