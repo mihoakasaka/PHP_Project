1fe842464fba42a7f8260e27f89cdb06c43b2c18
BH210-15\ipd <miho>
2017-11-09 14:22:30 -0500

    * working on FB log in

--- account.php
+++ account.php
@@ -80,7 +80,7 @@ $app->map('/passreset/token/:secretToken', function($secretToken) use ($app, $lo
         return;
     }
     if ($app->request()->isGet()) { // State 1: first show
-        $app->render('passreset_form.html.twig', array(
+        $app->render('account/passreset_form.html.twig', array(
             'name' => $user['name'], 'email' => $user['email']
         ));
     } else { // receiving POST with new password
@@ -102,7 +102,8 @@ $app->map('/passreset/token/:secretToken', function($secretToken) use ($app, $lo
                 'email' => $user['email']
             ));
         } else { // 2. successful submission
-            DB::update('users', array('password' => $pass1), 'id=%d', $user['id']);
+             $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
+            DB::update('users', array('password' => $passEnc), 'id=%d', $user['id']);
             $app->render('account/passreset_form_success.html.twig');
         }
     }
@@ -113,8 +114,11 @@ $app->get('/logout', function() use ($app) {
     $_SESSION['user'] = array();
     $app->render('account/logout.html.twig', array('userSession' => $_SESSION['user']));
 });
-//FB log in
 
+$app->get('/login', function() use ($app) {
+   
+    $app->render('account/login.html.twig', array('userSession' => $_SESSION['user']));
+});
 
 
 $app->post('/login', function() use ($app) {
@@ -205,76 +209,7 @@ $app->post('/register', function() use ($app) {
         $app->render('account/register_success.html.twig');
     }
 });
-/* * ***************************  log in with FB account ***************************** */
 
-
-require_once __DIR__ . '/Facebook/autoload.php';
-$fb = new Facebook\Facebook([
-    'app_id' => '291988171297988',
-    'app_secret' => '831a96ed04f3886d3eda5296e5d2ca34',
-    'default_graph_version' => 'v2.4',
-        ]);
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email']; // optional
-
-try {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $accessToken = $_SESSION['facebook_access_token'];
-    } else {
-        $accessToken = $helper->getAccessToken();
-    }
-} catch (Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch (Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
-if (isset($accessToken)) {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    } else {
-        // getting short-lived access token
-        $_SESSION['facebook_access_token'] = (string) $accessToken;
-        // OAuth 2.0 client handler
-        $oAuth2Client = $fb->getOAuth2Client();
-        // Exchanges a short-lived access token for a long-lived one
-        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-        // setting default access token to be used in script
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }
-    // redirect the user back to the same page if it has "code" GET variable
-    if (isset($_GET['code'])) {
-        header('Location: ./');
-    }
-    // getting basic info about user
-    try {
-        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-        $profile = $profile_request->getGraphNode()->asArray();
-    } catch (Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
-        echo 'Graph returned an error: ' . $e->getMessage();
-        session_destroy();
-        // redirecting user back to app login page
-        header("Location: ./");
-        exit;
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-    }
-
-    // printing $profile array on the screen which holds the basic info about user
-    print_r($profile);
-    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
-} else {
-    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-    $loginUrl = $helper->getLoginUrl('http://garagesale.ipd10.com/fblogincallback', $permissions);
-    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-}
 
 
 
---
+++ callback.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+require_once 'fbconfig.php';
+
+$helper = $fb->getRedirectLoginHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  if ($helper->getError()) {
+    header('HTTP/1.0 401 Unauthorized');
+    echo "Error: " . $helper->getError() . "\n";
+    echo "Error Code: " . $helper->getErrorCode() . "\n";
+    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+  } else {
+    header('HTTP/1.0 400 Bad Request');
+    echo 'Bad request';
+  }
+  exit;
+}
+$fb->setDefaultAccessToken($accessToken);
+
+	try {
+	  $response = $fb->get('/me?locale=en_US&fields=id,name,email,first_name,last_name');
+	  $userNode = $response->getGraphUser();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	  // When Graph returns an error
+	  echo 'Graph returned an error: ' . $e->getMessage();
+	  exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	  // When validation fails or other local issues
+	  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	  exit;
+	}
+
+	//echo 'Logged in as ' . $userNode->getName();
+        $fbUser = array(
+            'fName' => $userNode->getFirstName(),
+            'lName' => $userNode->getLastName(),
+            'email'=> $userNode->getEmail(),           
+            'ID' => $userNode->getId(),          
+            );
+
+        $_SESSION['facebook_access_token'] = $fbUser;
+        $_SESSION['user'] = array();
+        
+        header("Location: /");  
