f84061ddf7eaa097eb2484393798546ae8e1fac7
STUDENTS\8547102 <@8547102>
2017-11-10 14:28:13 -0500

    Project * Added styling to existing image display

--- cache/b0/b0dd7d517aa93f7cff3d175bf4a3cc9c083df19f4978d96fb48ae722737a7af6.php
+++ cache/b0/b0dd7d517aa93f7cff3d175bf4a3cc9c083df19f4978d96fb48ae722737a7af6.php
@@ -24,29 +24,36 @@ class __TwigTemplate_b6c07b804fe28db17ed6880f3b5118ce329cbcb765c4b2c27b8094a52df
         $context['_seq'] = twig_ensure_traversable((isset($context["i"]) ? $context["i"] : null));
         foreach ($context['_seq'] as $context["_key"] => $context["image"]) {
             // line 4
-            echo "        <img class=\"\" src=\"";
+            echo "    <div class=\"pict-thumb\">
+        <img  src=\"";
+            // line 5
             echo twig_escape_filter($this->env, $this->getAttribute($context["image"], "imagePath", array()), "html", null, true);
-            echo "\" />";
+            echo "\" />
+        ";
+            // line 6
             if ((twig_length_filter($this->env, (isset($context["i"]) ? $context["i"] : null)) > 1)) {
-                echo "<a href=\"/ajax/ad/";
+                // line 7
+                echo "            <a class=\"btn-danger\" href=\"/ajax/ad/";
                 echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "id", array()), "html", null, true);
                 echo "/pictures/delete/";
                 echo twig_escape_filter($this->env, $this->getAttribute($context["image"], "id", array()), "html", null, true);
                 echo "\">Delete Me</a> 
-        <a href=\"/ajax/ad/";
-                // line 5
+            <a class=\"btn-warning\" href=\"/ajax/ad/";
+                // line 8
                 echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "id", array()), "html", null, true);
                 echo "/pictures/delete/";
                 echo twig_escape_filter($this->env, $this->getAttribute($context["image"], "id", array()), "html", null, true);
-                echo "\">Make me the main picture</a>";
+                echo "\">Make me the main picture</a>
+        ";
             }
-            // line 6
-            echo "    ";
+            // line 10
+            echo "    </div>
+    ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['image'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 7
+        // line 12
         echo "</p>
 ";
     }
@@ -63,7 +70,7 @@ class __TwigTemplate_b6c07b804fe28db17ed6880f3b5118ce329cbcb765c4b2c27b8094a52df
 
     public function getDebugInfo()
     {
-        return array (  50 => 7,  44 => 6,  38 => 5,  27 => 4,  23 => 3,  19 => 1,);
+        return array (  57 => 12,  50 => 10,  43 => 8,  36 => 7,  34 => 6,  30 => 5,  27 => 4,  23 => 3,  19 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -79,8 +86,13 @@ class __TwigTemplate_b6c07b804fe28db17ed6880f3b5118ce329cbcb765c4b2c27b8094a52df
         return new Twig_Source("<h3>Existing images</h3>
 <p class=\"help-block\" id=\"existingImages\">
     {% for image in i %}
-        <img class=\"\" src=\"{{ image.imagePath }}\" />{% if i|length > 1%}<a href=\"/ajax/ad/{{v.id}}/pictures/delete/{{image.id}}\">Delete Me</a> 
-        <a href=\"/ajax/ad/{{v.id}}/pictures/delete/{{image.id}}\">Make me the main picture</a>{% endif %}
+    <div class=\"pict-thumb\">
+        <img  src=\"{{ image.imagePath }}\" />
+        {% if i|length > 1%}
+            <a class=\"btn-danger\" href=\"/ajax/ad/{{v.id}}/pictures/delete/{{image.id}}\">Delete Me</a> 
+            <a class=\"btn-warning\" href=\"/ajax/ad/{{v.id}}/pictures/delete/{{image.id}}\">Make me the main picture</a>
+        {% endif %}
+    </div>
     {% endfor %}
 </p>
 ", "existingAdImagesPanel.html.twig", "C:\\xampp\\htdocs\\phproject\\templates\\existingAdImagesPanel.html.twig");
