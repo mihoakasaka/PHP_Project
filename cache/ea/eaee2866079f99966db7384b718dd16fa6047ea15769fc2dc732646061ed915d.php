357cbd9e2fa1d427773174ec3c12b852346bc854
ipd <ipd@91792-HE139>
2017-11-12 12:09:59 -0500

    Project * fixed missplaced heading in search results

--- cache/ea/eaee2866079f99966db7384b718dd16fa6047ea15769fc2dc732646061ed915d.php
+++ cache/ea/eaee2866079f99966db7384b718dd16fa6047ea15769fc2dc732646061ed915d.php
@@ -44,52 +44,52 @@ class __TwigTemplate_3744c8db58ae6dbeea08c01705e38fec252e6c219615d5385128361541d
         echo "'/>
         <input type='submit' value='Search' />
     </form>
-    ";
+    <h2>Search results for <span class=\"quoted\">";
         // line 10
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "searchTerm", array()), "html", null, true);
+        echo "</span></h2>
+        ";
+        // line 11
         if ((isset($context["v"]) ? $context["v"] : null)) {
-            // line 11
-            echo "    <h3>Categories</h3>
-    <div id=\"searchResultsCategories\">
-        ";
-            // line 13
-            $this->loadTemplate("panelSearchResultsCategories.html.twig", "searchresults.html.twig", 13)->display($context);
+            // line 12
+            echo "        <h3>Categories</h3>
+        <div id=\"searchResultsCategories\">
+            ";
             // line 14
-            echo "    </div>
-    <ul id=\"searchResultsPaginationCategories\">
-        ";
-            // line 16
+            $this->loadTemplate("panelSearchResultsCategories.html.twig", "searchresults.html.twig", 14)->display($context);
+            // line 15
+            echo "        </div>
+        <ul id=\"searchResultsPaginationCategories\">
+            ";
+            // line 17
             $context['_parent'] = $context;
             $context['_seq'] = twig_ensure_traversable(range(1, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "maxCategoryPages", array())));
             foreach ($context['_seq'] as $context["_key"] => $context["page"]) {
-                // line 17
-                echo "            <li id=\"liPageCategory";
+                // line 18
+                echo "                <li id=\"liPageCategory";
                 echo twig_escape_filter($this->env, $context["page"], "html", null, true);
                 echo "\" ";
                 if (($context["page"] == $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "currentCategoryPage", array()))) {
                     echo "class=\"active\"";
                 }
                 echo ">
-                <a id=\"btPageCategory";
-                // line 18
+                    <a id=\"btPageCategory";
+                // line 19
                 echo twig_escape_filter($this->env, $context["page"], "html", null, true);
                 echo "\" onclick=\"loadCategoryPanel(";
                 echo twig_escape_filter($this->env, $context["page"], "html", null, true);
                 echo ");\">";
                 echo twig_escape_filter($this->env, $context["page"], "html", null, true);
                 echo "</a>
-            </li>
-        ";
+                </li>
+            ";
             }
             $_parent = $context['_parent'];
             unset($context['_seq'], $context['_iterated'], $context['_key'], $context['page'], $context['_parent'], $context['loop']);
             $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 21
-            echo "    </ul>
+            // line 22
+            echo "        </ul>
 
-        <h2>Search results for <span class=\"quoted\">";
-            // line 23
-            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["v"]) ? $context["v"] : null), "searchTerm", array()), "html", null, true);
-            echo "</span></h2>
         <h3>Ads</h3>
         <div id=\"searchResultsAds\"
              ";
@@ -136,7 +136,7 @@ class __TwigTemplate_3744c8db58ae6dbeea08c01705e38fec252e6c219615d5385128361541d
             $this->loadTemplate("panelSearchResultsUsers.html.twig", "searchresults.html.twig", 39)->display($context);
             // line 40
             echo "    </div>
-        <ul id=\"searchResultsPaginationUsers\">
+    <ul id=\"searchResultsPaginationUsers\">
         ";
             // line 42
             $context['_parent'] = $context;
@@ -239,7 +239,7 @@ class __TwigTemplate_3744c8db58ae6dbeea08c01705e38fec252e6c219615d5385128361541d
 
     public function getDebugInfo()
     {
-        return array (  219 => 77,  211 => 72,  205 => 69,  197 => 64,  191 => 61,  183 => 56,  179 => 54,  176 => 53,  168 => 47,  155 => 44,  146 => 43,  142 => 42,  138 => 40,  136 => 39,  129 => 34,  116 => 31,  107 => 30,  103 => 29,  99 => 27,  97 => 26,  91 => 23,  87 => 21,  74 => 18,  65 => 17,  61 => 16,  57 => 14,  55 => 13,  51 => 11,  49 => 10,  43 => 7,  39 => 5,  36 => 4,  30 => 2,  11 => 1,);
+        return array (  219 => 77,  211 => 72,  205 => 69,  197 => 64,  191 => 61,  183 => 56,  179 => 54,  176 => 53,  168 => 47,  155 => 44,  146 => 43,  142 => 42,  138 => 40,  136 => 39,  129 => 34,  116 => 31,  107 => 30,  103 => 29,  99 => 27,  97 => 26,  91 => 22,  78 => 19,  69 => 18,  65 => 17,  61 => 15,  59 => 14,  55 => 12,  53 => 11,  49 => 10,  43 => 7,  39 => 5,  36 => 4,  30 => 2,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -261,20 +261,20 @@ class __TwigTemplate_3744c8db58ae6dbeea08c01705e38fec252e6c219615d5385128361541d
         <input type='text' name='searchTerm' value='{{v.searchTerm}}'/>
         <input type='submit' value='Search' />
     </form>
-    {% if v %}
-    <h3>Categories</h3>
-    <div id=\"searchResultsCategories\">
-        {% include 'panelSearchResultsCategories.html.twig' %}
-    </div>
-    <ul id=\"searchResultsPaginationCategories\">
-        {% for page in range(1,v.maxCategoryPages) %}
-            <li id=\"liPageCategory{{page}}\" {% if page == v.currentCategoryPage %}class=\"active\"{% endif %}>
-                <a id=\"btPageCategory{{page}}\" onclick=\"loadCategoryPanel({{page}});\">{{page}}</a>
-            </li>
-        {% endfor %}
-    </ul>
+    <h2>Search results for <span class=\"quoted\">{{v.searchTerm}}</span></h2>
+        {% if v %}
+        <h3>Categories</h3>
+        <div id=\"searchResultsCategories\">
+            {% include 'panelSearchResultsCategories.html.twig' %}
+        </div>
+        <ul id=\"searchResultsPaginationCategories\">
+            {% for page in range(1,v.maxCategoryPages) %}
+                <li id=\"liPageCategory{{page}}\" {% if page == v.currentCategoryPage %}class=\"active\"{% endif %}>
+                    <a id=\"btPageCategory{{page}}\" onclick=\"loadCategoryPanel({{page}});\">{{page}}</a>
+                </li>
+            {% endfor %}
+        </ul>
 
-        <h2>Search results for <span class=\"quoted\">{{v.searchTerm}}</span></h2>
         <h3>Ads</h3>
         <div id=\"searchResultsAds\"
              {% include 'panelSearchResultsAds.html.twig' %}
@@ -292,7 +292,7 @@ class __TwigTemplate_3744c8db58ae6dbeea08c01705e38fec252e6c219615d5385128361541d
     <div id=\"searchResultsUsers\">
         {% include 'panelSearchResultsUsers.html.twig' %}
     </div>
-        <ul id=\"searchResultsPaginationUsers\">
+    <ul id=\"searchResultsPaginationUsers\">
         {% for page in range(1,v.maxUserPages) %}
             <li id=\"liPageUser{{page}}\" {% if page == v.currentUserPage %}class=\"active\"{% endif %}>
                 <a id=\"btPageUser{{page}}\" onclick=\"loadUserPanel({{page}});\">{{page}}</a>
